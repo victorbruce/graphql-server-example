6db6c081b3d4e9276eda656440bfe30c1eedfa78
Victor Bruce <victorbruce82@gmail.com>
2024-01-17 12:05:12 +0000

    perform mutation(delete and add) on the game schema

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -25,19 +25,33 @@ export const resolvers = {
   Game: {
     reviews(parent) {
       return db.reviews.filter((review) => review.game_id === parent.id);
-    }
+    },
   },
   Author: {
     reviews(parent) {
       return db.reviews.filter((review) => review.author_id === parent.id);
-    }
+    },
   },
   Review: {
     author(parent) {
-      return db.authors.find((author) => author.id === parent.author_id)
+      return db.authors.find((author) => author.id === parent.author_id);
     },
     game(parent) {
-      return db.games.find((game) => game.id === parent.game_id)
+      return db.games.find((game) => game.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((game) => game.id !== args.id);
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000)
+      }
+      db.games.push(game)
+      return game
     }
-  }
+  },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -29,4 +29,13 @@ export const typeDefs = `#graphql
 		authors: [Author]
 		author(id: ID!): Author
 	}
+
+	type Mutation {
+		addGame(game: AddGameInput!): Game
+		deleteGame(id: ID!): [Game]
+	}
+	input AddGameInput {
+		title: String!
+		platform: [String!]!
+	}
 `
